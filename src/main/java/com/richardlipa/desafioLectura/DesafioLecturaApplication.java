e0339ef47eb4a8bb06e5762636234c668ce28328
Richard Lipa <rlipac31@gmail.com>
2025-06-10 15:38:25 -0500

    first commit

---
+++ src/main/java/com/richardlipa/desafioLectura/DesafioLecturaApplication.java
@@ -0,0 +1,13 @@
+package com.richardlipa.desafioLectura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioLecturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioLecturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/richardlipa/desafioLectura/DesafioLecturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richardlipa.desafioLectura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioLecturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
